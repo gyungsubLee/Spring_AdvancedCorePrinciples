d34af95f5c8934b7eeb051c8c750122ef6fc04e7
gyoungsubLee <dltjq2323@gmail.com>
2024-11-10 16:50:45 +0900

    project init

---
+++ src/main/java/com/inflearn/lecture_prac/LecturePracApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.lecture_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecturePracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LecturePracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inflearn/lecture_prac/LecturePracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.lecture_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecturePracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
