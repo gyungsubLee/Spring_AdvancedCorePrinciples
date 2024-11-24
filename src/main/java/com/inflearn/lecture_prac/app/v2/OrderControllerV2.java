8fb22634201924c2b70d859b9d2bccc2129542bb
gyoungsubLee <dltjq2323@gmail.com>
2024-11-24 17:16:30 +0900

    Chapter 1.5 - 로그 추적기 V2 적용

---
+++ src/main/java/com/inflearn/lecture_prac/app/v2/OrderControllerV2.java
@@ -0,0 +1,30 @@
+package com.inflearn.lecture_prac.app.v2;
+
+import com.inflearn.lecture_prac.trace.HelloTraceV2;
+import com.inflearn.lecture_prac.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(@RequestParam String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
---
+++ src/main/java/com/inflearn/lecture_prac/app/v2/OrderServiceV2.java
@@ -0,0 +1,27 @@
+package com.inflearn.lecture_prac.app.v2;
+
+import com.inflearn.lecture_prac.trace.HelloTraceV2;
+import com.inflearn.lecture_prac.trace.TraceId;
+import com.inflearn.lecture_prac.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
