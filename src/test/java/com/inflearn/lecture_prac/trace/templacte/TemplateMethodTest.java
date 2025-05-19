c94a2172a7852f1a300dbe9e53a3e268cba3675d
gyoungsubLee <dltjq2323@gmail.com>
2025-05-19 20:09:52 +0900

    Chapter 3.3 - 템플릿 메서드 패턴, 익명 내부 클래스로 구현

--- src/test/java/com/inflearn/lecture_prac/trace/templacte/TemplateMethodTest.java
+++ src/test/java/com/inflearn/lecture_prac/trace/templacte/TemplateMethodTest.java
@@ -43,4 +43,28 @@ public class TemplateMethodTest {
         AbstractTemplate template2 = new SubClassLogic1();
         template2.execute();
     }
+
+    /**
+     * 템플릿 메서드 패턴, 익명 내부 클래스 사용
+     */
+    @Test
+    void templateMethodV2() {
+        SubClassLogic1.AbstractTemplate template1 = new SubClassLogic1.AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1 {}", template1.getClass());
+        template1.execute();
+
+        SubClassLogic1.AbstractTemplate template2 = new SubClassLogic1.AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2 {}", template2.getClass());
+        template2.execute();
+    }
 }
