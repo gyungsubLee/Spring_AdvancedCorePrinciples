eaf59c73aad04933a2438afe3b0d4f5138a6d45a
gyoungsubLee <dltjq2323@gmail.com>
2025-05-21 14:50:56 +0900

    Chapter 4.2 - 기본 예제2 ( 인터페이스 제거, 빈 수동 등록 )

--- src/main/java/com/inflearn/lecture_prac/LecturePracApplication.java
+++ src/main/java/com/inflearn/lecture_prac/LecturePracApplication.java
@@ -1,21 +1,18 @@
 package com.inflearn.lecture_prac;
 
 import com.inflearn.lecture_prac.proxy.config.AppV1Config;
+import com.inflearn.lecture_prac.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 
-@Import(AppV1Config.class)
-@SpringBootApplication(scanBasePackages = "com.inflearn.lecture_prac.proxy.app.v1")
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "com.inflearn.lecture_prac.proxy.app")
 //@ComponentScan(basePackages = "com.inflearn.lecture_prac.template_strategy")
 public class LecturePracApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LecturePracApplication.class, args);
     }
-
 }
-
-
-
