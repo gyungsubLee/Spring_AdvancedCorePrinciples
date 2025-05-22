61504ad5ab6cea1bdae2691fccfaa87eb2b40be4
gyoungsubLee <dltjq2323@gmail.com>
2025-05-22 19:42:16 +0900

    Chapter 4.6 - 데코레이터 패턴 실습: 패턴 적용

--- src/test/java/com/inflearn/lecture_prac/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/com/inflearn/lecture_prac/pureproxy/decorator/DecoratorPatternTest.java
@@ -2,6 +2,7 @@ package com.inflearn.lecture_prac.pureproxy.decorator;
 
 import com.inflearn.lecture_prac.pureproxy.decorator.code.Component;
 import com.inflearn.lecture_prac.pureproxy.decorator.code.DecoratorPatternClient;
+import com.inflearn.lecture_prac.pureproxy.decorator.code.MessageDecorator;
 import com.inflearn.lecture_prac.pureproxy.decorator.code.RealComponent;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,14 @@ public class DecoratorPatternTest {
     void noDecorator() {
         Component realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
         client.execute();
     }
 }
