b8c4503c84abff5e83db979a4eed1b9943878773
gyoungsubLee <dltjq2323@gmail.com>
2025-05-22 19:10:47 +0900

    Chapter 4.4 - 프록시 패턴 실습: 패턴 적용

--- src/test/java/com/inflearn/lecture_prac/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/com/inflearn/lecture_prac/pureproxy/proxy/ProxyPatternTest.java
@@ -1,7 +1,9 @@
 package com.inflearn.lecture_prac.pureproxy.proxy;
 
+import com.inflearn.lecture_prac.pureproxy.proxy.code.CacheProxy;
 import com.inflearn.lecture_prac.pureproxy.proxy.code.ProxyPatternClient;
 import com.inflearn.lecture_prac.pureproxy.proxy.code.RealSubject;
+import com.inflearn.lecture_prac.pureproxy.proxy.code.Subject;
 import org.junit.jupiter.api.Test;
 
 public class ProxyPatternTest {
@@ -10,6 +12,16 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.excute();
+        client.excute();
+        client.excute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        Subject realSubject = new RealSubject();
+        Subject cacheSubject = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheSubject);
         client.excute();
         client.excute();
         client.excute();
