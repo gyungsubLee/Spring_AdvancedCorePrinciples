5755d71b28ec735fdf93e2b74f44968aee50c736
gyoungsubLee <dltjq2323@gmail.com>
2025-05-25 15:29:48 +0900

    Chapter 5.2 - JDK 동적 프록시: 기본 예제

---
+++ src/test/java/com/inflearn/lecture_prac/jdkdynamic/code/AInterface.java
@@ -0,0 +1,5 @@
+package com.inflearn.lecture_prac.jdkdynamic.code;
+
+public interface AInterface {
+    String call();
+}
---
+++ src/test/java/com/inflearn/lecture_prac/jdkdynamic/code/BInterface.java
@@ -0,0 +1,5 @@
+package com.inflearn.lecture_prac.jdkdynamic.code;
+
+public interface BInterface {
+    String call();
+}
