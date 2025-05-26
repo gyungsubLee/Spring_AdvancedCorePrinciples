cc110389ae005d41a625815e42109996652a3765
gyoungsubLee <dltjq2323@gmail.com>
2025-05-26 19:24:11 +0900

    fix: OrderControllerConcreteProxy 순환 참조
    문제
    - 새로운 예제를 추가하면서 이전에 학습을 위해 등록한ConcreteProxyConfig를 스캔 범위에서 제외함
    - 이로 인해 수동 빈으로 등록한 구체 클래스가 스캔되지 않아 이를 상속하는 OrderControllerConcreteProxy를 빈으로 주입 ( 다형성 )
    - 따라서 자기 자신을 참조하는 순환 참조 문제가 발생됨
    
    해결
    - OrderControllerConcreteProxy를 스캔 범위에서 제거함 -> @RestController 주석 처리

--- src/main/java/com/inflearn/lecture_prac/proxy/app/v1_proxy/concrete_proxy/OrderControllerConcreteProxy.java
+++ src/main/java/com/inflearn/lecture_prac/proxy/app/v1_proxy/concrete_proxy/OrderControllerConcreteProxy.java
@@ -6,7 +6,12 @@ import com.inflearn.lecture_prac.proxy.app.basic_ex.v2.OrderControllerV2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+// NOTES: 순환 참조 문제가 발생 되어 임시로 주석 처리함
+    // 이유: target2를 등록한 ConcreteProxyConfig를 스캔 범위에서 제외함
+    //      따라서 수동 빈으로 등록하던 concrete class인 OrderControllerV2가 빈으로 등록되지 않아
+    //      이를 상속하는 OrderControllerConcreteProxy를 빈으로 주입함 ( 다형성 )
+    //      이로 인해 자기 자신을 참조하는 순환 참조 문제가 발생됨.
+//@RestController
 public class OrderControllerConcreteProxy extends OrderControllerV2 {
 
     private final OrderControllerV2 target;
