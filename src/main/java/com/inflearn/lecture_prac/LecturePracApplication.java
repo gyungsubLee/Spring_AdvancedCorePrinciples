d50fbbe855204c6e4d61bd5084cfd19ddcb31b19
gyoungsubLee <dltjq2323@gmail.com>
2025-05-27 11:47:38 +0900

    Chapter 5.5 - JDK 동적 프록시 앱 적용 2
    - Filter를 통해 부가기능(로그 추적)을 적용에 대해 필터링함

--- src/main/java/com/inflearn/lecture_prac/LecturePracApplication.java
+++ src/main/java/com/inflearn/lecture_prac/LecturePracApplication.java
@@ -3,14 +3,15 @@ package com.inflearn.lecture_prac;
 import com.inflearn.lecture_prac.proxy.app.basic_ex.trace.logtrace.LogTrace;
 import com.inflearn.lecture_prac.proxy.app.basic_ex.trace.logtrace.ThreadLocalLogTrace;
 import com.inflearn.lecture_prac.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import com.inflearn.lecture_prac.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 
-//@Import(ConcreteProxyConfig.class)
-@Import(DynamicProxyBasicConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "com.inflearn.lecture_prac.proxy.app")
 public class LecturePracApplication {
 
